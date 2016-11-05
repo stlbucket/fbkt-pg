c02a5af64ff27cac60ef6a892b79faabed4a0f63
stlbucket <stlbucket@gmail.com>
2016-11-04 19:43:01 -0700

    cleanup

--- pgRestApi/serverExtensions/pgRestApi/initQueryViewControllers/index.js
+++ pgRestApi/serverExtensions/pgRestApi/initQueryViewControllers/index.js
@@ -3,10 +3,8 @@ const R = require('ramda');
 const Promise = require('bluebird');
 const fbkt = require('fbkt');
 
-var getQueryEntityConfigs = require('./getQueryEntityConfigs');
 var configureFbktQueryEndpoint = require('./configureFbktQueryEndpoint');
 var configureEntityQueryEndpoint = require('./configureEntityQueryEndpoint');
-// var configureUserEntityFilterEndpoint = require('./configureUserEntityFilterEndpoint');
 
 
 module.exports = (callInfo)=> {
