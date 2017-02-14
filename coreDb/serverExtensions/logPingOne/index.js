40db95e92109fcb97bb4ba9c81410ece00000fef
stlbucket <stlbucket@gmail.com>
2017-02-14 09:32:28 -0600

    fix ping emitter unit test

--- coreDb/serverExtensions/logPingOne/index.js
+++ coreDb/serverExtensions/logPingOne/index.js
@@ -5,9 +5,10 @@ module.exports = ()=> {
   const pingOneEmitter = fbkt().libs.restApi.eventEmitters.ping().on(
     'PING_ONE',
     e=>{
+      console.log('PING EMITTED', e);
       fbkt().dbTree.fbkt_core_db.table.ping_request.save({
         params: {
-          uid:  e.params.id
+          uid:  e
         }
       })
   });
