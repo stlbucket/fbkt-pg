533135bc94e8e09f6e95f67d31c252d16c897c30
stlbucket <stlbucket@gmail.com>
2017-03-10 08:25:10 -0600

    config

--- config/index.js
+++ config/index.js
@@ -3,6 +3,8 @@ module.exports = {
   appRouteFilter: process.env.APP_ROUTE_FILTER,
 	restErrorMode: process.env.REST_ERROR_MODE,
   defaultEntityControllerAuth: process.env.DEFAULT_ENTITY_CONTROLLER_AUTH,
+  apiVersion: process.env.API_VERSION,
+  enableGraphIql: process.env.ENABLE_GRAPH_IQL,
   dbAccess:	{
 		client: process.env.DB_CLIENT,
     connection: process.env.DB_CONNECTION,
