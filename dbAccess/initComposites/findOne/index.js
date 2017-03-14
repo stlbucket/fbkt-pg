ec58526a4bdc93b9a1e2dc7fcb9afb252684c7a3
stlbucket <stlbucket@gmail.com>
2017-03-14 09:28:59 -0500

    Revert "1.0.33"
    This reverts commit 0a999199e83e46ee18d30b43998c736fdc5d3e76.

--- dbAccess/initComposites/findOne/index.js
+++ dbAccess/initComposites/findOne/index.js
@@ -1,69 +1,29 @@
 "use strict";
-const R       = require('ramda');
+const R = require('ramda');
 const Promise = require('bluebird');
-const fbkt    = require('fbkt');
+const fbkt = require('fbkt');
 
 
 module.exports = R.curry((composite, callInfo)=> {
-  // fbkt().clog('FIND ONE COMPOSITE', callInfo, true);
-  // fbkt().clog('FIND ONE COMPOSITE', composite, true);
-  const ci = {
-    user: callInfo.user,
-    debugQuery: callInfo.debugQuery,
-    params: Object.assign(callInfo.params, {composite: composite})
-  };
-  // fbkt().clog('ci', ci, true);
-
-  return fbkt().FbktPipe({
-    name: 'initComposite.findOne',
-    filename: __filename,
-    expectedParams: {},
-    pipelineParams: {},
-    pipelineSteps: {  // any number of functions
-      "findOne": function (callInfo) {
-        fbkt().clog('FIND ONE COMPOSITE', callInfo, true);
-        return fbkt()
-          .dbTree[callInfo.params.composite.schemaName]
-          .view[callInfo.params.composite.tableName + '_composite_view']
-          .findOne(callInfo.params)
-          .then(function (viewResult) {
-            fbkt().clog('viewResult', viewResult);
-            return viewResult ?
-              R.is(Error, viewResult) ? viewResult : viewResult
-              : null;
-          });
-      }
-    }
-  }, ci);
+	return fbkt().FbktPipe({
+		name:           'initComposite.findOne',
+		filename:       __filename,
+		expectedParams: {},
+		pipelineParams: {},
+		pipelineSteps: {  // any number of functions
+			"findOne": function (callInfo) {
+				fbkt().clog('FIND ONE COMPOSITE', callInfo);
+				return fbkt()
+					.dbTree[callInfo.params.composite.schemaName]
+					.view[callInfo.params.composite.tableName + '_composite_view']
+					.findOne(callInfo.params)
+					.then(function (viewResult) {
+						fbkt().clog('viewResult', viewResult);
+						return viewResult ?
+							R.is(Error, viewResult) ? viewResult : viewResult
+							: null;
+					});
+			}
+		}
+	}, R.mergeWith(R.mergeWith, callInfo, {params: {composite: composite}}));
 });
-
-
-// "use strict";
-// const R = require('ramda');
-// const Promise = require('bluebird');
-// const fbkt = require('fbkt');
-//
-//
-// module.exports = R.curry((composite, callInfo)=> {
-// 	return fbkt().FbktPipe({
-// 		name:           'initComposite.findOne',
-// 		filename:       __filename,
-// 		expectedParams: {},
-// 		pipelineParams: {},
-// 		pipelineSteps: {  // any number of functions
-// 			"findOne": function (callInfo) {
-// 				fbkt().clog('FIND ONE COMPOSITE', callInfo);
-// 				return fbkt()
-// 					.dbTree[callInfo.params.composite.schemaName]
-// 					.view[callInfo.params.composite.tableName + '_composite_view']
-// 					.findOne(callInfo.params)
-// 					.then(function (viewResult) {
-// 						fbkt().clog('viewResult', viewResult);
-// 						return viewResult ?
-// 							R.is(Error, viewResult) ? viewResult : viewResult
-// 							: null;
-// 					});
-// 			}
-// 		}
-// 	}, R.mergeWith(R.mergeWith, callInfo, {params: {composite: composite}}));
-// });
